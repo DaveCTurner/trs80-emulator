72fb2423687963ce7615addd98066be25ea73949
Dave Turner <dave.c.turner@gmail.com>
2009-10-18 09:04:41 +0100

    Added Program.asJava() method.

--- src/dct25/trs80/syntaxTree/Program.java
+++ src/dct25/trs80/syntaxTree/Program.java
@@ -27,6 +27,34 @@ public class Program extends beaver.Symbol {
         return sb.toString();
     }
     
+    public String asJava() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("package dct25.trs80.examplePrograms.onTheFly;\n");
+        sb.append("\n");
+        sb.append("import dct25.trs80.emulator.Environment;\n");
+        sb.append("import dct25.trs80.emulator.Executable;\n");
+        sb.append("\n");
+        sb.append("/**\n");
+        sb.append(" * Translation of the following TRS-80 BASIC program into Java:\n");
+        sb.append(" * \n");
+        sb.append(" * ---\n");
+        sb.append("10 CLS\n");
+        sb.append(" * ---\n");
+        sb.append(" */\n");
+        sb.append("\n");
+        sb.append("public class OnTheFlyProgram implements Executable {\n");
+        sb.append("\n");
+        sb.append("    /**\n");
+        sb.append("     * Execute the program.\n");
+        sb.append("     * @param env The environment in which to execute the program.\n");
+        sb.append("     */\n");
+        sb.append("    public void execute(Environment env) {\n");
+        sb.append("        env.clearScreen();\n");
+        sb.append("    }\n");
+        sb.append("}\n");
+        return sb.toString();
+    }
+    
     public boolean equals(Object o) {
         if (this == o) { return true; }
         if (o == null) { return false; }
