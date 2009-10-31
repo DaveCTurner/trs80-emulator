ae9e7bcb721dce9fe9c74ce054801579638ca470
Dave Turner <dave.c.turner@gmail.com>
2009-10-31 10:22:19 +0000

    BasicToJavaCompiler.generateCode() uses Eclipse AST rather than string concatenation to build program.

--- src/dct25/trs80/emulator/BasicToJavaCompiler.java
+++ src/dct25/trs80/emulator/BasicToJavaCompiler.java
@@ -1,5 +1,7 @@
 package dct25.trs80.emulator;
 
+import org.eclipse.jdt.core.dom.*;
+
 import dct25.trs80.inMemoryCompiler.InMemorySourceCompiler;
 import dct25.trs80.syntaxTree.Program;
 
@@ -18,32 +20,75 @@ public class BasicToJavaCompiler {
         return compiler.instantiate();
     }
 
+    @SuppressWarnings("unchecked")
     public String generateCode(Program program) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("package "); sb.append(_packageName); sb.append(";\n");
-        sb.append("\n");
-        sb.append("import dct25.trs80.emulator.Environment;\n");
-        sb.append("import dct25.trs80.emulator.Executable;\n");
-        sb.append("\n");
-        sb.append("/**\n");
-        sb.append(" * Translation of the following TRS-80 BASIC program into Java:\n");
-        sb.append(" * \n");
-        sb.append(" * ---\n");
-        sb.append(program.asBasic());
-        sb.append(" * ---\n");
-        sb.append(" */\n");
-        sb.append("\n");
-        sb.append("public class " + _className + " implements Executable {\n");
-        sb.append("\n");
-        sb.append("    /**\n");
-        sb.append("     * Execute the program.\n");
-        sb.append("     * @param env The environment in which to execute the program.\n");
-        sb.append("     */\n");
-        sb.append("    public void execute(Environment env) {\n");
-        sb.append("        env.clearScreen();\n");
-        sb.append("    }\n");
-        sb.append("}\n");
-        return sb.toString();
+        AST ast = AST.newAST(AST.JLS3);
+        CompilationUnit cu = ast.newCompilationUnit();
+        
+        PackageDeclaration pkDec = ast.newPackageDeclaration();
+        cu.setPackage(pkDec);
+        pkDec.setName(ast.newName(_packageName));
+        
+        ImportDeclaration imEnvironment = ast.newImportDeclaration();
+        cu.imports().add(imEnvironment);
+        imEnvironment.setName(ast.newName("dct25.trs80.emulator.Environment"));
+
+        ImportDeclaration imExecutable = ast.newImportDeclaration();
+        cu.imports().add(imExecutable);
+        imExecutable.setName(ast.newName("dct25.trs80.emulator.Executable"));
+        
+        TypeDeclaration tyProgram = ast.newTypeDeclaration();
+        cu.types().add(tyProgram);
+        tyProgram.setName(ast.newSimpleName(_className));
+        tyProgram.modifiers().add(ast.newModifier(Modifier.ModifierKeyword.PUBLIC_KEYWORD));
+        tyProgram.superInterfaceTypes().add(ast.newSimpleType(ast.newSimpleName("Executable")));
+        
+        Javadoc jdProgram = ast.newJavadoc();
+        tyProgram.setJavadoc(jdProgram);
+        TagElement jdProgramTextTag = ast.newTagElement();
+        jdProgram.tags().add(jdProgramTextTag);
+        TextElement jdProgramText = ast.newTextElement();
+        jdProgramTextTag.fragments().add(jdProgramText);
+        jdProgramText.setText("Translation of the following TRS-80 Program into Java:\n---\n"
+                + program.asBasic() + "---");
+        
+        MethodDeclaration medExecute = ast.newMethodDeclaration();
+        tyProgram.bodyDeclarations().add(medExecute);
+        medExecute.setName(ast.newSimpleName("execute"));
+        medExecute.setReturnType2(ast.newPrimitiveType(PrimitiveType.VOID));
+        medExecute.modifiers().add(ast.newModifier(Modifier.ModifierKeyword.PUBLIC_KEYWORD));
+        
+        Javadoc jdMedExecute = ast.newJavadoc();
+        medExecute.setJavadoc(jdMedExecute);
+        TagElement jdMedExecuteTextTag = ast.newTagElement();
+        jdMedExecute.tags().add(jdMedExecuteTextTag);
+        TextElement jdMedExecuteText = ast.newTextElement();
+        jdMedExecuteTextTag.fragments().add(jdMedExecuteText);
+        jdMedExecuteText.setText("Execute the program");
+
+        TagElement jdMedExecuteParamTag = ast.newTagElement();
+        jdMedExecute.tags().add(jdMedExecuteParamTag);
+        jdMedExecuteParamTag.setTagName(TagElement.TAG_PARAM);
+        TextElement jdMedExecuteParam = ast.newTextElement();
+        jdMedExecuteParamTag.fragments().add(ast.newSimpleName("env"));
+        jdMedExecuteParamTag.fragments().add(jdMedExecuteParam);
+        jdMedExecuteParam.setText(" The environment in which to execute the program.");
+        
+        SingleVariableDeclaration medExecuteParam = ast.newSingleVariableDeclaration();
+        medExecute.parameters().add(medExecuteParam);
+        medExecuteParam.setName(ast.newSimpleName("env"));
+        medExecuteParam.setType(ast.newSimpleType(ast.newSimpleName("Environment")));
+        
+        Block medExecuteBody = ast.newBlock();
+        medExecute.setBody(medExecuteBody);
+        
+        MethodInvocation clsInvocation = ast.newMethodInvocation();
+        Statement clsStatement = ast.newExpressionStatement(clsInvocation);
+        medExecuteBody.statements().add(clsStatement);
+        clsInvocation.setExpression(ast.newSimpleName("env"));
+        clsInvocation.setName(ast.newSimpleName("clearScreen"));
+
+        return cu.toString();
     }
 
 }
