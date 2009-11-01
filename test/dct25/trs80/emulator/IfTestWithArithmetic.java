3929af34d7fb82cb799b86af90e03d7cf3aa0611
Dave Turner <dave.c.turner@gmail.com>
2009-11-01 16:00:54 +0000

    Added simple test of arithmetic

---
+++ test/dct25/trs80/emulator/IfTestWithArithmetic.java
@@ -0,0 +1,48 @@
+package dct25.trs80.emulator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import dct25.trs80.syntaxTree.BooleanExpression;
+import dct25.trs80.syntaxTree.ClearScreenStatement;
+import dct25.trs80.syntaxTree.IfStatement;
+import dct25.trs80.syntaxTree.IntegerExpression;
+import dct25.trs80.syntaxTree.IntegerLiteral;
+import dct25.trs80.syntaxTree.LineNumber;
+import dct25.trs80.syntaxTree.Program;
+import dct25.trs80.syntaxTree.ProgramLine;
+import dct25.trs80.syntaxTree.Statement;
+
+public class IfTestWithArithmetic {
+    
+    @Test
+    public void shouldSkipLine() throws Exception {
+        Program p = new Program(new ProgramLine[] {
+                new ProgramLine(new LineNumber(10), new Statement[] {
+                    new IfStatement(BooleanExpression.equals(
+                            IntegerExpression.fromIntegerLiteral(new IntegerLiteral(3)),
+                            IntegerExpression.sum(
+                                    IntegerExpression.fromIntegerLiteral(new IntegerLiteral(1)),
+                                    IntegerExpression.fromIntegerLiteral(new IntegerLiteral(2))
+                            )
+                    ), new LineNumber(30))
+                }),
+                
+                new ProgramLine(new LineNumber(20), new Statement[] {
+                    new ClearScreenStatement()
+                }),
+                
+                new ProgramLine(new LineNumber(30), new Statement[] {
+                    new ClearScreenStatement()
+                })
+        });
+        
+        Executable e = new BasicToJavaCompiler("OnTheFlyProgram", "dct25.trs80.examplePrograms.onTheFly").compile(p);
+
+        InstrumentedEnvironment env = new InstrumentedEnvironment();
+        assertEquals("Should not have cleared screen", 0, env.getScreenClearedCount());
+        e.execute(env);
+        assertEquals("Should now have cleared screen once", 1, env.getScreenClearedCount());
+    }
+}
