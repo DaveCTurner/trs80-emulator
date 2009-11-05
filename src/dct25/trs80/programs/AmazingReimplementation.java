eb659d8f46149f8d675de11e20c1722973e94fc0
Dave Turner <dave.c.turner@gmail.com>
2009-11-05 21:48:58 +0000

    Implemented printMaze method to replace code from line 1200 onwards.

--- src/dct25/trs80/programs/AmazingReimplementation.java
+++ src/dct25/trs80/programs/AmazingReimplementation.java
@@ -2,34 +2,28 @@ package dct25.trs80.programs;
 
 public class AmazingReimplementation extends amazing_test_bas {
     protected void line1200statement0() {
-        J = (1);
-        do {
+        printMaze(V, H, Vs);
+    }
+
+    private void printMaze(int v, int h, int[][] wallFlags) {
+        for (int j = 0; j < v; j++) {
             _env.print("I", false);
-            I = (1);
-            do {
-                if (((Vs[I-1][J-1]) < (2))) {
+            for (int i = 0; i < h; i++) {
+                if ((wallFlags[i][j] & 0x02) == 0) {
                     _env.print("  I", false);
                 } else {
                     _env.print("   ", false);
                 }
-                I += 1;
-            } while (I <= H);
+            }
             _env.print(" ", true);
-            I = (1);
-            do {
-                if (((Vs[I-1][J-1]) == (0))) {
+            for (int i = 0; i < h; i++) {
+                if ((wallFlags[i][j] & 0x01) == 0) {
                     _env.print(":--", false);
                 } else {
-                    if (((Vs[I-1][J-1]) == (2))) {
-                        _env.print(":--", false);
-                    } else {
-                        _env.print(":  ", false);
-                    }
+                    _env.print(":  ", false);
                 }
-                I += 1;
-            } while (I <= (H));
+            }
             _env.print(":", true);
-            J += 1;
-        } while (J <= V);
+        }
     }
 }
