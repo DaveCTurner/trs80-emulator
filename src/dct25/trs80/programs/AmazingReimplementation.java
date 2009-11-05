5636063480be2038f43192d10dcb6bf9e676ba03
Dave Turner <dave.c.turner@gmail.com>
2009-11-05 22:08:49 +0000

    Reimplemented program startup, to line 100.

--- src/dct25/trs80/programs/AmazingReimplementation.java
+++ src/dct25/trs80/programs/AmazingReimplementation.java
@@ -1,6 +1,31 @@
 package dct25.trs80.programs;
 
+import dct25.trs80.emulator.Environment;
+
 public class AmazingReimplementation extends amazing_test_bas {
+    public void execute(Environment env) {
+        _env = env;
+        _env.clearScreen();
+        _env.print("AMAZING", true);
+        _env.print("COPYRIGHT BY", true);
+        _env.print("CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY", true);
+        II = 1501; // replaced FOR II = (1) TO (1500) : NEXT
+        while (true) {
+            _env.clearScreen();
+            _env.print("WHAT ARE YOUR WIDTH AND LENGTH", true);
+            H = _env.getInput();
+            V = _env.getInput();
+            if ((((H) != (1)) && ((V) != (1)))) {
+                break;
+            }
+            _env.print("MEANINGLESS DIMENSIONS. TRY AGAIN", true);
+            A = 501; // replaced FOR A=1 TO 500: NEXT A
+        }
+        _env.print("PRINTOUT IS IN PROGRESS, PLEASE BE PATIENT", true);
+        line100statement0();
+    }
+
+    
     protected void line1200statement0() {
         printMaze(V, H, Vs);
     }
