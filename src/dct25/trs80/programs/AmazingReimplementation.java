cbb147ce698ebd1f4756e70a9eca6f8dce565fd3
Dave Turner <dave.c.turner@gmail.com>
2009-11-05 22:19:20 +0000

    More tidying up of startup section - re-ordered 'if's. Inlined to line 110.

--- src/dct25/trs80/programs/AmazingReimplementation.java
+++ src/dct25/trs80/programs/AmazingReimplementation.java
@@ -9,23 +9,29 @@ public class AmazingReimplementation extends amazing_test_bas {
         _env.print("AMAZING", true);
         _env.print("COPYRIGHT BY", true);
         _env.print("CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY", true);
-        II = 1501; // replaced FOR II = (1) TO (1500) : NEXT
-        while (true) {
+
+        boolean valid = false;
+        int h, v;
+        h = v = 0;
+        while (!valid) {
             _env.clearScreen();
             _env.print("WHAT ARE YOUR WIDTH AND LENGTH", true);
-            H = _env.getInput();
-            V = _env.getInput();
-            if ((((H) != (1)) && ((V) != (1)))) {
-                break;
+            h = _env.getInput();
+            v = _env.getInput();
+            valid = !((h == 1) || (v == 1));
+            if (!valid) {
+                _env.print("MEANINGLESS DIMENSIONS. TRY AGAIN", true);
             }
-            _env.print("MEANINGLESS DIMENSIONS. TRY AGAIN", true);
-            A = 501; // replaced FOR A=1 TO 500: NEXT A
         }
         _env.print("PRINTOUT IS IN PROGRESS, PLEASE BE PATIENT", true);
-        line100statement0();
+
+        II = 1501; // replaced FOR II = (1) TO (1500) : NEXT
+        A = 501; // replaced FOR A=1 TO 500: NEXT A
+        H = h;
+        V = v;
+        line110statement0();
     }
 
-    
     protected void line1200statement0() {
         printMaze(V, H, Vs);
     }
